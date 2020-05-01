fbc7f80abc58d706c31c7276d9790eb880c326a1
pallavi <pallavi.thanikonda@gmail.com>
2020-05-01 20:27:38 +0200

    1. Installed react-hook-form for validations 2. Added Sing-up form validations 3. Updated App.css for error message color

--- frontend-communityfirst/src/component/auth/RegisterForm.js
+++ frontend-communityfirst/src/component/auth/RegisterForm.js
@@ -1,58 +1,75 @@
 import React, {useState} from "react";
+import { useForm } from "react-hook-form";
+
 
 function RegisterForm({onSubmit}) {
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    const { handleSubmit, register, watch, errors } = useForm();
+    const onClick = data => {
+        console.log(data);
+    };
     return (
         <div className="card">
             <div className="card-body">
                 <h4 className="card-title">Become a Member</h4>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-row">
                         <div className="col-md-6">
                             <div className="form-group">
-                                <input
+                                <input name ="firstname"
                                     type="text"
                                     className="form-control form-control-lg"
                                     value={firstname}
                                     onChange={e => setFirstname(e.target.value)}
-                                    placeholder="Firstname"/>
+                                    placeholder="Firstname"
+                                    ref={register({ required: true, minLength: 3, maxLength: 20  })} />
+                                    {errors.firstname && <p>Firstname is required and size must be between 3 and 20 </p>}
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
-                                <input
+                                <input name="lastname"
                                     type="text"
                                     className="form-control form-control-lg"
                                     value={lastname}
                                     onChange={e => setLastname(e.target.value)}
-                                    placeholder="lastname"/>
+                                    placeholder="lastname"
+                                    ref={register({ required: true, minLength: 3, maxLength: 20 })} />
+                                    {errors.lastname && <p>Lastname is required and size must be between 3 and 20 </p>}
                             </div>
                         </div>
                     </div>
                     <div className="form-group">
-                        <input
+                        <input name="email"
                             type="email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                             className="form-control form-control-lg"
-                            placeholder="Email"/>
+                            placeholder="Email"
+                            ref={register({ required: true,
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                                }
+                            })}/>
+                            {errors.email && <p>Email is required and Must be a well-formed email address </p>}
                     </div>
                     <div className="form-group">
-                        <input
+                        <input name="password"
                             type="password"
                             placeholder="Enter your password"
                             className="form-control form-control-lg"
                             value={password}
-                            onChange={e => setPassword(e.target.value)}/>
+                            onChange={e => setPassword(e.target.value)}
+                            ref={register({ required: true, minLength: 5, maxLength: 50 })} />
+                            {errors.password && <p>Password is required and size must be between 5 and 50 </p>}
                     </div>
                     <button
                         type="submit"
                         className="btn btn-primary"
-                        onClick={(e) => {
+                        onSubmit={(e) => {
                             e.preventDefault();
                             onSubmit({firstname, lastname, email, password});
                         }}>
