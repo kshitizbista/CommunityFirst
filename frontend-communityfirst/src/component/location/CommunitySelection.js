7bbc91879cd1391c5e2f8bf76095374669cf34d8
kshitizbista <tsetiz.bista@gmail.com>
2020-05-06 00:00:26 +0200

    added css class to show pointer on hover on community selection dropdown

--- frontend-communityfirst/src/component/location/CommunitySelection.js
+++ frontend-communityfirst/src/component/location/CommunitySelection.js
@@ -9,13 +9,13 @@ function CommunitySelection({cities, communities, onCityClick, onCommunityClick}
         <div className="row justify-content-center">
             <div className="col-auto">
                 <div className="dropdown">
-                    <span className="btn btn-info dropdown-toggle btn-lg" role="button" id="dropdownMenuLink"
+                    <span className="btn btn-info dropdown-toggle btn-lg" role="button" id="cityDropdownMenuLink"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {selectedCity}
                     </span>
 
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        {cities.map((city) => <span key={city.id} className="dropdown-item"
+                    <div className="dropdown-menu" aria-labelledby="cityDropdownMenuLink">
+                        {cities.map((city) => <span key={city.id} className="dropdown-item community-selection"
                                                     onClick={() => {
                                                         onCityClick(city.id)
                                                         setSelectedCity(city.name)
@@ -23,20 +23,20 @@ function CommunitySelection({cities, communities, onCityClick, onCommunityClick}
                     </div>
                 </div>
             </div>
-            <div className="col-auto">
+            { communities.length > 0 && <div className="col-auto">
                 <div className="dropdown">
-                    <span className="btn btn-info dropdown-toggle btn-lg" role="button" id="dropdownMenuLink"
+                    <span className="btn btn-info dropdown-toggle btn-lg" role="button" id="communityDropdownMenuLink"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {selectedCommunity}
                     </span>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        {communities.map((community) => <span key={community.id} className="dropdown-item"
+                    <div className="dropdown-menu" aria-labelledby="communityDropdownMenuLink">
+                        {communities.map((community) => <span key={community.id} className="dropdown-item community-selection"
                                                               onClick={() => {
                                                                   onCommunityClick(selectedCity, community.name)
                                                               }}>{community.name}</span>)}
                     </div>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
