7b5573c0831a6b2a91f21decd901735b172d8af4
ainurapolatoglu <ainura.dyuss@gmail.com>
2020-05-06 17:56:53 +0200

    Tests Item - getAll #5

---
+++ src/test/java/se/sda/communityfirst/item/ItemControllerTest.java
@@ -0,0 +1,75 @@
+package se.sda.communityfirst.item;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import se.sda.communityfirst.items.Item;
+import se.sda.communityfirst.items.ItemController;
+import se.sda.communityfirst.items.ItemService;
+import se.sda.communityfirst.location.Community;
+import se.sda.communityfirst.user.User;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class ItemControllerTest {
+    @Mock
+    ItemService itemService;
+
+    @InjectMocks
+    ItemController itemController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(itemController).build();
+    }
+
+    @Test
+    void getAll() throws Exception {
+        List<Item> items = new ArrayList<>();
+
+        Item dress = new Item();
+        dress.setId(1L);
+        dress.setTitle("Foo");
+        dress.setText("Lorem ipsum");
+        dress.setOffering(true);
+
+        User user = new User();
+        user.setId(1L);
+        dress.setUser(user);
+
+        Community taby = new Community();
+        taby.setId(1L);
+        dress.setCommunity(taby);
+
+        items.add(dress);
+
+        when(itemService.getAll()).thenReturn(items);
+        mockMvc.perform(get(ItemController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[*]", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is (1)))
+                .andExpect(jsonPath("$[0].title", equalTo("Foo")))
+                .andExpect(jsonPath("$[0].text", equalTo("Lorem ipsum")))
+                .andExpect(jsonPath("$[0].offering", is(true)))
+                .andExpect(jsonPath("$[0].user.id", is (1)))
+                .andExpect(jsonPath("$[0].community.id", is (1)));
+        verify(itemService).getAll();
+    }
+
+
+}
---
+++ src/test/java/se/sda/communityfirst/item/ItemServiceTest.java
@@ -0,0 +1,4 @@
+package se.sda.communityfirst.item;
+
+public class ItemServiceTest {
+}
