9532d97a71f5b667e16878974fac931b0e1533b8
ainurapolatoglu <ainura.dyuss@gmail.com>
2020-05-11 01:20:33 +0200

    Removed the notnullable constraint of city_id from users table. Added a SQL file to help dropping the tables.

---
+++ src/main/resources/drop.sql
@@ -0,0 +1,14 @@
+drop table assistance;
+drop table items;
+drop table users;
+drop table communities;
+drop table cities;
+drop table databasechangelog;
+drop table databasechangeloglock;
+drop sequence assistance_id_seq;
+drop sequence items_id_seq;
+drop sequence cities_id_seq;
+drop sequence communities_id_seq;
+drop sequence users_id_seq;
+
+commit;
