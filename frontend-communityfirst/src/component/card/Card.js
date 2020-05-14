0f75684e8b0a615ac71f6f664fc29e8d559cf500
PireIre <bozoire@gmail.com>
2020-05-14 10:25:35 +0200

    refactor code in MyPost/ edit and delete functionality now work for items

--- frontend-communityfirst/src/component/card/Card.js
+++ frontend-communityfirst/src/component/card/Card.js
@@ -1,18 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import {assistanceType} from "../post/PostCreation";
 import format from "date-fns/format";
-import {Modal, Button, Form, ButtonGroup} from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 function Card(props) {
 
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     let deleteBtn = null;
     if (props.showDelete) {
-        deleteBtn = (<Button type="button" className="close" variant="primary" onClick={handleShow} aria-label="Close">
+        deleteBtn = (<Button type="button" className="close" variant="primary" onClick={() => props.onDelete(props.id)} aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </Button>)
     }
@@ -25,21 +20,6 @@ function Card(props) {
         </Button>)
     }
 
-    const modal = (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Delete</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
-            <Modal.Footer>
-                <Button className="btn btn-danger" variant="primary" onClick={() => {
-                    props.onDelete(props.id);
-                    handleClose();
-                }}>Delete</Button>
-                <Button variant="secondary" onClick={handleClose}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
 
     return (
         <>
@@ -85,7 +65,6 @@ function Card(props) {
                             {editBtn}
                         </div>
                     </div>
-                    {modal}
                 </div>
             </div>
         </>
--- frontend-communityfirst/src/component/card/ItemCard.js
+++ frontend-communityfirst/src/component/card/ItemCard.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { itemType } from "../post/ItemPostCreation";
+import {assistanceType} from "../post/PostCreation";
 import format from "date-fns/format";
+import {Button} from "react-bootstrap";
 
 function ItemCard(props) {
+
+    let deleteBtn = null;
+    if (props.showDelete) {
+        deleteBtn = (<Button type="button" className="close" variant="primary" onClick={() => props.onDelete(props.id)} aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </Button>)
+    }
+
+    let editBtn = null;
+    if (props.showEdit) {
+        editBtn = (<Button type="button" className="close" variant="primary"
+                           onClick={() => props.onEdit(props.id, props.title, props.description)} aria-label="Close">
+            <span aria-hidden="true">Edit</span>
+        </Button>)
+    }
+
+
     return (
         <>
-            <div onClick={props.delete} className="card mb-2">
+            <div className="card mb-2">
                 <div className="card-body p-2">
                     <div className="row mt-2">
                         <div className="col-auto pr-0">
@@ -23,14 +41,14 @@ function ItemCard(props) {
                                 </div>
                                 <div className="col-auto align-self-center">
                                     <p className="font-weight-bolder mb-0"
-                                        style={{ "fontSize": "13px" }}>
+                                       style={{"fontSize": "13px"}}>
                                         {props.firstname} {props.lastname}
                                     </p>
                                     <p className="text-primary font-weight-light mb-0"
-                                        style={{ "fontSize": "12px" }}>
-                                        {props.serviceType === itemType.REQUEST_HELP ? 'Requesting Help' : 'Offering Help'}</p>
+                                       style={{"fontSize": "12px"}}>
+                                        {props.serviceType === assistanceType.REQUEST_HELP ? 'Requesting Help' : 'Offering Help'}</p>
                                     <p className="text-secondary font-weight-light mb-0"
-                                        style={{ "fontSize": "12px" }}>
+                                       style={{"fontSize": "12px"}}>
                                         {format(new Date(props.postedDate), "MMMM dd, yyyy")}
                                     </p>
                                 </div>
@@ -39,8 +57,12 @@ function ItemCard(props) {
 
                         <div className="col-lg">
                             <h5 className="text-secondary font-weight-bold mb-1"
-                                style={{ "fontSize": "14px" }}>{props.title}</h5>
-                            <p className="m-0" style={{ "fontSize": "14px" }}>{props.description}</p>
+                                style={{"fontSize": "14px"}}>{props.title}</h5>
+                            <p className="m-0" style={{"fontSize": "14px"}}>{props.description}</p>
+                        </div>
+                        <div className="col-sm">
+                            {deleteBtn}
+                            {editBtn}
                         </div>
                     </div>
                 </div>
@@ -50,3 +72,4 @@ function ItemCard(props) {
 }
 
 export default ItemCard;
+
