f3fe0229f29d0e546fdc271ba60e2694d256efa8
krishnanadella <nadellalk@gmail.com>
2020-05-15 15:07:07 +0200

    added service details page to support SeeMore functionality

--- frontend-communityfirst/src/component/post/ServiceDetail.js
+++ frontend-communityfirst/src/component/post/ServiceDetail.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import PostApi from "../../api/PostApi";
 import format from "date-fns/format";
+import Card from "../card/Card";
 
 function ServiceDetail() {
 
@@ -23,13 +24,21 @@ function ServiceDetail() {
     }, []);
 
     return (
-        <div>
-            <p>Title: {post.title}</p>
+        <div>                        
+            <Card   key={post.title}
+                    description={post.description}
+                    serviceType={post.assistanceType}
+                    postedDate={post.postedDate}
+                    email={post.email}
+                    firstname={post.firstname}
+                    lastname={post.lastname}
+            />
+            {/* <p>Title: {post.title}</p>
             <p>Description: {post.description}</p>
             <p>Service Type:{post.assistanceTypes}</p>
             <p>{post.postedDate}</p>
             <p>Name: {post.firstname} {post.lastname}</p>
-            <p>Email: {post.email}</p>
+            <p>Email: {post.email}</p> */}
         </div>
     )
 }
