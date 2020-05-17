a1e4787929edbfd4ce078380f26af5eed0ca6ba0
ainurapolatoglu <ainura.dyuss@gmail.com>
2020-05-17 16:00:51 +0200

    Merge conflict fixed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ DELETE from cities;
 
 DELETE from cities;
 INSERT INTO cities (id,name) VALUES (1,'Stockholm');
+INSERT INTO communities(id, name, city_id) values (1, 'Kungsholmen', 1);
 INSERT INTO communities(id, name, city_id) values (2, 'Östermalm', 1);
 INSERT INTO communities(id, name, city_id) values (3, 'Södermalm', 1);
 INSERT INTO communities(id, name, city_id) values (4, 'Bromma', 1);
