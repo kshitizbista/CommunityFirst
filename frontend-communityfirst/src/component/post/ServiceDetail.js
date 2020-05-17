e41679688d4197f8767211d5140e6e50649e501c
krishnanadella <nadellalk@gmail.com>
2020-05-17 13:48:37 +0200

    Added card layout to servicedetail page

--- frontend-communityfirst/src/component/post/ServiceDetail.js
+++ frontend-communityfirst/src/component/post/ServiceDetail.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
 import PostApi from "../../api/PostApi";
 import format from "date-fns/format";
+import { Link,useParams, useHistory} from "react-router-dom";
 
 function ServiceDetail() {
 
     const {id} = useParams();
     const [post, setPost] = useState({});
+    const history = useHistory();
 
     const getPostById = async () => {
         try {
@@ -23,13 +24,17 @@ function ServiceDetail() {
     }, []);
 
     return (
-        <div>
-            <p>Title: {post.title}</p>
-            <p>Description: {post.description}</p>
-            <p>Service Type:{post.assistanceTypes}</p>
-            <p>{post.postedDate}</p>
-            <p>Name: {post.firstname} {post.lastname}</p>
-            <p>Email: {post.email}</p>
+        <div className="card card-layout">
+            <div className="card-body">
+                <h3 className="card-title"><b><i>{post.title}</i></b></h3>
+                <p className="card-text word-wrap"><b>Description:</b> {post.description}</p>
+                <p className="card-text"><b>Posted By:</b> {post.firstname} {post.lastname}</p>
+                <p className="card-text"><b>Contact Details:</b> {post.email}</p>
+            </div>
+            <div className="card-footer text-muted">
+                <p>Posted on: {post.postedDate}</p>
+                <button className="btn btn-primary ml-1" onClick={() =>history.push("/communityfirst/se/services")} >Back to Services</button>          
+            </div>
         </div>
     )
 }
